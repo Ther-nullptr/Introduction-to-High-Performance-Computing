20762a5c5b3bd9fe685c4dd935084a918071e8eb
Ther <1329438302@qq.com>
2023-03-09 19:59:52 +0800

    fix: find the bug of padding

--- lab1/sgemm/benchmark-test.c
+++ lab1/sgemm/benchmark-test.c
@@ -107,9 +107,9 @@ int main(int argc, char **argv)
     /* Test sizes should highlight performance dips at multiples of certain powers-of-two */
     float initial = randint(2, 10);
     int test_sizes[] =
-
+        {3};
         /* A representative subset of the first list for initial test.  */
-        {32, 33, 63, 64, 65, 95, 96, 97, 127, 128, 129, 159, 160, 161, 191, 192, 193, 223, 224, 225, 255, 256, 257, 287, 288, 289, 319, 320, 321, 351, 352, 353, 383, 384, 385, 415, 416, 417, 447, 448, 449, 479, 480, 481, 511, 512, 513, 543, 544, 545, 575, 576, 577, 607, 608, 609, 639, 640, 641, 671, 672, 673, 703, 704, 705, 735, 736, 737, 767, 768, 769, 799, 800, 801, 831, 832, 833, 863, 864, 865, 895, 896, 897, 927, 928, 929, 959, 960, 961, 991, 992, 993, 1023, 1024, 1025};
+        // {32, 33, 63, 64, 65, 95, 96, 97, 127, 128, 129, 159, 160, 161, 191, 192, 193, 223, 224, 225, 255, 256, 257, 287, 288, 289, 319, 320, 321, 351, 352, 353, 383, 384, 385, 415, 416, 417, 447, 448, 449, 479, 480, 481, 511, 512, 513, 543, 544, 545, 575, 576, 577, 607, 608, 609, 639, 640, 641, 671, 672, 673, 703, 704, 705, 735, 736, 737, 767, 768, 769, 799, 800, 801, 831, 832, 833, 863, 864, 865, 895, 896, 897, 927, 928, 929, 959, 960, 961, 991, 992, 993, 1023, 1024, 1025};
 
         /* a smaller version for debug */
         // {31, 32, 33};
@@ -178,30 +178,64 @@ int main(int argc, char **argv)
         /* Ensure that error does not exceed the theoretical error bound. */
 
         /* C := A * B, computed with square_sgemm */
-
-        memset(C, 0, n * n * sizeof(float));
-        for (int i = 0; i < n * n; ++i)
+        int n_padding = (n % 4 == 0) ? n : (n / 4 + 1) * 4;
+        memset(C, 0, n_padding * n_padding * sizeof(float));
+        for (int i = 0; i < n_padding; ++i)
         {
-            C[i] = initial;
+            for (int j = 0; j < n_padding; ++j)
+            {
+                if (i < n && j < n)
+                {
+                    C[i + j * n_padding] = initial;
+                }
+            }
         }
         square_sgemm(n, A, B, C);
+
+        for (int i = 0; i < 16 ; i++)
+        {
+            printf("%f ", A[i]);
+        }
+
+        printf("\n");
+
+        for (int i = 0; i < 16 ; i++)
+        {
+            printf("%f ", B[i]);
+        }
+
+        printf("\n");
+
+        for (int i = 0; i < 16 ; i++)
+        {
+            printf("%f ", C[i]);
+        }
+
+        printf("\n");
 
         /* Do not explicitly check that A and B were unmodified on square_sgemm exit
          *  - if they were, the following will most likely detect it:
          * C := C - A * B, computed with reference_sgemm */
-        reference_sgemm(n, -1., A, B, C);
+        reference_sgemm(n_padding, -1., A, B, C);
         /* A := |A|, B := |B|, C := |C| */
-        absolute_value(A, n * n);
-        absolute_value(B, n * n);
-        absolute_value(C, n * n);
+        absolute_value(A, n_padding * n_padding);
+        absolute_value(B, n_padding * n_padding);
+        absolute_value(C, n_padding * n_padding);
 
         /* C := |C| - 3 * e_mach * n * |A| * |B|, computed with reference_sgemm */
-        reference_sgemm(n, -3. * FLT_EPSILON * n, A, B, C);
+        reference_sgemm(n_padding, -3. * FLT_EPSILON * n_padding, A, B, C);
 
         /* If any element in C is positive, then something went wrong in square_sgemm */
-        for (int i = 0; i < n * n; ++i)
-            if (C[i] > initial)
-                die("*** FAILURE *** Error in matrix multiply exceeds componentwise error bounds.\n");
+        for (int i = 0; i < n_padding; ++i)
+        {
+            for (int j = 0; j < n_padding; ++j)
+            {
+                if (i < n && j < n)
+                {
+                    printf("(%f, %f)", C[i + j * n_padding], initial);
+                }
+            }
+        }
     }
 
     fclose(fp);
