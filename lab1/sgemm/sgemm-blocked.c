d547e7348f6ea9e4fc71cc51c0bd12ddf3852d91
Ther <1329438302@qq.com>
2023-03-09 16:21:03 +0800

    chore: add code of register settings

--- lab1/sgemm/sgemm-blocked.c
+++ lab1/sgemm/sgemm-blocked.c
@@ -7,38 +7,109 @@ const char *sgemm_desc = "Simple blocked sgemm.";
 #endif
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
+#define A(i,j) A[ (j)*lda + (i) ]
+#define B(i,j) B[ (j)*lda + (i) ]
+#define C(i,j) C[ (j)*lda + (i) ]
 
 /* This auxiliary subroutine performs a smaller sgemm operation
  *  C := C + A * B
  * where C is M-by-N, A is M-by-K, and B is K-by-N. 
 */
-static inline void do_block_divide_unrolling_a(int lda, int M, int N, int K, float *A, float *B, float *C)
+static inline void do_block_divide_4x4(int lda, int M, int N, int K, float *A, float *B, float *C)
 {
     /* For each row i of A */
-    for (int i = 0; i < M; ++i)
+    for (int i = 0; i < M; i += 4)
     {
         /* For each column j of B */
         for (int j = 0; j < N; j += 4)
         {
             /* Compute C(i,j) */
-            float cij_0 = C[i + j * lda];
-            float cij_1 = C[i + (j + 1) * lda];
-            float cij_2 = C[i + (j + 2) * lda];
-            float cij_3 = C[i + (j + 3) * lda];
+            register float c_00 = 0., c_01 = 0., c_02 = 0., c_03 = 0.,
+                           c_10 = 0., c_11 = 0., c_12 = 0., c_13 = 0.,
+                           c_20 = 0., c_21 = 0., c_22 = 0., c_23 = 0.,
+                           c_30 = 0., c_31 = 0., c_32 = 0., c_33 = 0.;
+            register float a_0k, a_1k, a_2k, a_3k;
+            register float b_k0, b_k1, b_k2, b_k3;
+
+            c_00 = C(i, j);
+            c_10 = C(i, j + 1);
+            c_20 = C(i, j + 2);
+            c_30 = C(i, j + 3);
+
+            c_01 = C(i + 1, j);
+            c_11 = C(i + 1, j + 1);
+            c_21 = C(i + 1, j + 2);
+            c_31 = C(i + 1, j + 3);
+
+            c_02 = C(i + 2, j);
+            c_12 = C(i + 2, j + 1);
+            c_22 = C(i + 2, j + 2);
+            c_32 = C(i + 2, j + 3);
+
+            c_03 = C(i + 3, j);
+            c_13 = C(i + 3, j + 1);
+            c_23 = C(i + 3, j + 2);
+            c_33 = C(i + 3, j + 3);
+
+            float *p_b_k0, *p_b_k1, *p_b_k2, *p_b_k3;
+
+            p_b_k0 = &B(0, 0 + j);
+            p_b_k1 = &B(0, 1 + j);
+            p_b_k2 = &B(0, 2 + j);
+            p_b_k3 = &B(0, 3 + j);
 
             for (int k = 0; k < K; ++k)
             {
-                float aik = A[i + k * lda];
-                cij_0 += aik * B[k + j * lda];
-                cij_1 += aik * B[k + (j + 1) * lda];
-                cij_2 += aik * B[k + (j + 2) * lda];
-                cij_3 += aik * B[k + (j + 3) * lda];
+                a_0k = A(0 + i, k);
+                a_1k = A(1 + i, k);
+                a_2k = A(2 + i, k);
+                a_3k = A(3 + i, k);
+
+                b_k0 = *p_b_k0++;
+                b_k1 = *p_b_k1++;
+                b_k2 = *p_b_k2++;
+                b_k3 = *p_b_k3++;
+
+                c_00 += a_0k * b_k0;
+                c_01 += a_0k * b_k1;
+                c_02 += a_0k * b_k2;
+                c_03 += a_0k * b_k3;
+
+                c_10 += a_1k * b_k0;
+                c_11 += a_1k * b_k1;
+                c_12 += a_1k * b_k2;
+                c_13 += a_1k * b_k3;
+
+                c_20 += a_2k * b_k0;
+                c_21 += a_2k * b_k1;
+                c_22 += a_2k * b_k2;
+                c_23 += a_2k * b_k3;
+
+                c_30 += a_3k * b_k0;
+                c_31 += a_3k * b_k1;
+                c_32 += a_3k * b_k2;
+                c_33 += a_3k * b_k3;
             }
 
-            C[i + j * lda] = cij_0;
-            C[i + (j + 1) * lda] = cij_1;
-            C[i + (j + 2) * lda] = cij_2;
-            C[i + (j + 3) * lda] = cij_3;
+            C(i, j) = c_00;
+            C(i, j + 1) = c_10;
+            C(i, j + 2) = c_20;
+            C(i, j + 3) = c_30;
+
+            C(i + 1, j) = c_01;
+            C(i + 1, j + 1) = c_11;
+            C(i + 1, j + 2) = c_21;
+            C(i + 1, j + 3) = c_31;
+
+            C(i + 2, j) = c_02;
+            C(i + 2, j + 1) = c_12;
+            C(i + 2, j + 2) = c_22;
+            C(i + 2, j + 3) = c_32;
+
+            C(i + 3, j) = c_03;
+            C(i + 3, j + 1) = c_13;
+            C(i + 3, j + 2) = c_23;
+            C(i + 3, j + 3) = c_33;
         }
     }
 }
@@ -65,7 +136,7 @@ void square_sgemm(int lda, float *A, float *B, float *C)
                 int K = min(BLOCK_SIZE, lda - k);
 
                 /* Perform individual block sgemm */
-                do_block_divide_unrolling_a(lda, M, N, K, A + i + k * lda, B + k + j * lda, C + i + j * lda);
+                do_block_divide_4x4(lda, M, N, K, A + i + k * lda, B + k + j * lda, C + i + j * lda);
             }
         }
     }
