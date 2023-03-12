b2d1de8efa03a9b0692e2494a8e4ea1c202306b4
Ther <1329438302@qq.com>
2023-03-12 23:44:34 +0800

    chore: add naive simd

--- lab1/sgemm/sgemm-blocked.c
+++ lab1/sgemm/sgemm-blocked.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <immintrin.h>
 
 const char *sgemm_desc = "Simple blocked sgemm.";
 
@@ -7,15 +8,15 @@ const char *sgemm_desc = "Simple blocked sgemm.";
 #endif
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
-#define A(i,j) A[ (j)*lda + (i) ]
-#define B(i,j) B[ (j)*lda + (i) ]
-#define C(i,j) C[ (j)*lda + (i) ]
+#define A(i, j) A[(j)*lda + (i)]
+#define B(i, j) B[(j)*lda + (i)]
+#define C(i, j) C[(j)*lda + (i)]
 
 /* This auxiliary subroutine performs a smaller sgemm operation
  *  C := C + A * B
- * where C is M-by-N, A is M-by-K, and B is K-by-N. 
-*/
-static inline void do_block_divide_4x4(int lda, int M, int N, int K, float *A, float *B, float *C)
+ * where C is M-by-N, A is M-by-K, and B is K-by-N.
+ */
+static inline void do_block_divide_simd(int lda, int M, int N, int K, float *A, float *B, float *C)
 {
     /* For each row i of A */
     for (int i = 0; i < M;)
@@ -27,34 +28,6 @@ static inline void do_block_divide_4x4(int lda, int M, int N, int K, float *A, f
             {
                 if (N - j >= 4)
                 {
-                    /* Compute C(i,j) */
-                    register float c_00 = 0., c_01 = 0., c_02 = 0., c_03 = 0.,
-                        c_10 = 0., c_11 = 0., c_12 = 0., c_13 = 0.,
-                        c_20 = 0., c_21 = 0., c_22 = 0., c_23 = 0.,
-                        c_30 = 0., c_31 = 0., c_32 = 0., c_33 = 0.;
-                    register float a_0k, a_1k, a_2k, a_3k;
-                    register float b_k0, b_k1, b_k2, b_k3;
-
-                    c_00 = C(i, j);
-                    c_01 = C(i, j + 1);
-                    c_02 = C(i, j + 2);
-                    c_03 = C(i, j + 3);
-
-                    c_10 = C(i + 1, j);
-                    c_11 = C(i + 1, j + 1);
-                    c_12 = C(i + 1, j + 2);
-                    c_13 = C(i + 1, j + 3);
-
-                    c_20 = C(i + 2, j);
-                    c_21 = C(i + 2, j + 1);
-                    c_22 = C(i + 2, j + 2);
-                    c_23 = C(i + 2, j + 3);
-
-                    c_30 = C(i + 3, j);
-                    c_31 = C(i + 3, j + 1);
-                    c_32 = C(i + 3, j + 2);
-                    c_33 = C(i + 3, j + 3);
-
                     float *p_b_k0, *p_b_k1, *p_b_k2, *p_b_k3;
 
                     p_b_k0 = &B(0, 0 + j);
@@ -62,87 +35,126 @@ static inline void do_block_divide_4x4(int lda, int M, int N, int K, float *A, f
                     p_b_k2 = &B(0, 2 + j);
                     p_b_k3 = &B(0, 3 + j);
 
-                    for (int k = 0; k < K; ++k)
+                    for (int k = 0; k < K;)
                     {
-                        a_0k = A(0 + i, k);
-                        a_1k = A(1 + i, k);
-                        a_2k = A(2 + i, k);
-                        a_3k = A(3 + i, k);
+                        if (K - k >= 4)
+                        {
+                            __m128 a_0 = _mm_loadu_ps(&A(i, k)); // a_0k, a_1k, a_2k, a_3k
+                            __m128 a_1 = _mm_loadu_ps(&A(i, k + 1));
+                            __m128 a_2 = _mm_loadu_ps(&A(i, k + 2));
+                            __m128 a_3 = _mm_loadu_ps(&A(i, k + 3));
 
-                        b_k0 = *p_b_k0;
-                        p_b_k0++;
-                        b_k1 = *p_b_k1;
-                        p_b_k1++;
-                        b_k2 = *p_b_k2;
-                        p_b_k2++;
-                        b_k3 = *p_b_k3;
-                        p_b_k3++;
+                            // the position of c_00, c_10, c_20, c_30 is continuous
+                            __m128 b_00 = _mm_set1_ps(*p_b_k0); // b_1j, b_1j, b_1j, b_1j
+                            __m128 b_10 = _mm_set1_ps(*p_b_k1); // b_2j, b_2j, b_2j, b_2j
+                            __m128 b_20 = _mm_set1_ps(*p_b_k2); // b_3j, b_3j, b_3j, b_3j
+                            __m128 b_30 = _mm_set1_ps(*p_b_k3); // b_4j, b_4j, b_4j, b_4j
 
-                        c_00 += a_0k * b_k0;
-                        c_01 += a_0k * b_k1;
-                        c_02 += a_0k * b_k2;
-                        c_03 += a_0k * b_k3;
+                            __m128 b_01 = _mm_set1_ps(*(p_b_k0 + 1)); // b_1j, b_1j, b_1j, b_1j
+                            __m128 b_11 = _mm_set1_ps(*(p_b_k1 + 1)); // b_2j, b_2j, b_2j, b_2j
+                            __m128 b_21 = _mm_set1_ps(*(p_b_k2 + 1)); // b_3j, b_3j, b_3j, b_3j
+                            __m128 b_31 = _mm_set1_ps(*(p_b_k3 + 1)); // b_4j, b_4j, b_4j, b_4j
 
-                        c_10 += a_1k * b_k0;
-                        c_11 += a_1k * b_k1;
-                        c_12 += a_1k * b_k2;
-                        c_13 += a_1k * b_k3;
+                            __m128 b_02 = _mm_set1_ps(*(p_b_k0 + 2)); // b_1j, b_1j, b_1j, b_1j
+                            __m128 b_12 = _mm_set1_ps(*(p_b_k1 + 2)); // b_2j, b_2j, b_2j, b_2j
+                            __m128 b_22 = _mm_set1_ps(*(p_b_k2 + 2)); // b_3j, b_3j, b_3j, b_3j
+                            __m128 b_32 = _mm_set1_ps(*(p_b_k3 + 2)); // b_4j, b_4j, b_4j, b_4j
 
-                        c_20 += a_2k * b_k0;
-                        c_21 += a_2k * b_k1;
-                        c_22 += a_2k * b_k2;
-                        c_23 += a_2k * b_k3;
+                            __m128 b_03 = _mm_set1_ps(*(p_b_k0 + 3)); // b_1j, b_1j, b_1j, b_1j
+                            __m128 b_13 = _mm_set1_ps(*(p_b_k1 + 3)); // b_2j, b_2j, b_2j, b_2j
+                            __m128 b_23 = _mm_set1_ps(*(p_b_k2 + 3)); // b_3j, b_3j, b_3j, b_3j
+                            __m128 b_33 = _mm_set1_ps(*(p_b_k3 + 3)); // b_4j, b_4j, b_4j, b_4j
 
-                        c_30 += a_3k * b_k0;
-                        c_31 += a_3k * b_k1;
-                        c_32 += a_3k * b_k2;
-                        c_33 += a_3k * b_k3;
+                            p_b_k0 += 4;
+                            p_b_k1 += 4;
+                            p_b_k2 += 4;
+                            p_b_k3 += 4;
+
+                            __m128 c_0 = _mm_loadu_ps(&C(i, 0 + j)); // c_00, c_10, c_20, c_30
+                            __m128 c_1 = _mm_loadu_ps(&C(i, 1 + j)); // c_01, c_11, c_21, c_31
+                            __m128 c_2 = _mm_loadu_ps(&C(i, 2 + j)); // c_02, c_12, c_22, c_32
+                            __m128 c_3 = _mm_loadu_ps(&C(i, 3 + j)); // c_03, c_13, c_23, c_33
+
+                            c_0 = _mm_add_ps(c_0,
+                                             _mm_add_ps(
+                                                 _mm_add_ps(_mm_mul_ps(a_0, b_00), _mm_mul_ps(a_1, b_01)),
+                                                 _mm_add_ps(_mm_mul_ps(a_2, b_02), _mm_mul_ps(a_3, b_03))));
+
+                            c_1 = _mm_add_ps(c_1,
+                                             _mm_add_ps(
+                                                 _mm_add_ps(_mm_mul_ps(a_0, b_10), _mm_mul_ps(a_1, b_11)),
+                                                 _mm_add_ps(_mm_mul_ps(a_2, b_12), _mm_mul_ps(a_3, b_13))));
+
+                            c_2 = _mm_add_ps(c_2,
+                                             _mm_add_ps(
+                                                 _mm_add_ps(_mm_mul_ps(a_0, b_20), _mm_mul_ps(a_1, b_21)),
+                                                 _mm_add_ps(_mm_mul_ps(a_2, b_22), _mm_mul_ps(a_3, b_23))));
+
+                            c_3 = _mm_add_ps(c_3,
+                                             _mm_add_ps(
+                                                 _mm_add_ps(_mm_mul_ps(a_0, b_30), _mm_mul_ps(a_1, b_31)),
+                                                 _mm_add_ps(_mm_mul_ps(a_2, b_32), _mm_mul_ps(a_3, b_33))));
+
+                            _mm_storeu_ps(&C(i, 0 + j), c_0);
+                            _mm_storeu_ps(&C(i, 1 + j), c_1);
+                            _mm_storeu_ps(&C(i, 2 + j), c_2);
+                            _mm_storeu_ps(&C(i, 3 + j), c_3);
+
+                            k += 4;
+                        }
+                        else
+                        {
+                            __m128 a = _mm_loadu_ps(&A(i, k)); // a_0k, a_1k, a_2k, a_3k
+
+                            // the position of c_00, c_10, c_20, c_30 is continuous
+                            __m128 b_0 = _mm_set1_ps(*p_b_k0); // b_1j, b_1j, b_1j, b_1j
+                            __m128 b_1 = _mm_set1_ps(*p_b_k1); // b_2j, b_2j, b_2j, b_2j
+                            __m128 b_2 = _mm_set1_ps(*p_b_k2); // b_3j, b_3j, b_3j, b_3j
+                            __m128 b_3 = _mm_set1_ps(*p_b_k3); // b_4j, b_4j, b_4j, b_4j
+
+                            p_b_k0++;
+                            p_b_k1++;
+                            p_b_k2++;
+                            p_b_k3++;
+
+                            __m128 c_0 = _mm_loadu_ps(&C(i, 0 + j)); // c_00, c_10, c_20, c_30
+                            __m128 c_1 = _mm_loadu_ps(&C(i, 1 + j)); // c_01, c_11, c_21, c_31
+                            __m128 c_2 = _mm_loadu_ps(&C(i, 2 + j)); // c_02, c_12, c_22, c_32
+                            __m128 c_3 = _mm_loadu_ps(&C(i, 3 + j)); // c_03, c_13, c_23, c_33
+
+                            c_0 = _mm_add_ps(c_0, _mm_mul_ps(a, b_0));
+                            c_1 = _mm_add_ps(c_1, _mm_mul_ps(a, b_1));
+                            c_2 = _mm_add_ps(c_2, _mm_mul_ps(a, b_2));
+                            c_3 = _mm_add_ps(c_3, _mm_mul_ps(a, b_3));
+
+                            _mm_storeu_ps(&C(i, 0 + j), c_0);
+                            _mm_storeu_ps(&C(i, 1 + j), c_1);
+                            _mm_storeu_ps(&C(i, 2 + j), c_2);
+                            _mm_storeu_ps(&C(i, 3 + j), c_3);
+
+                            k++;
+                        }
                     }
-
-                    C(i, j) = c_00;
-                    C(i, j + 1) = c_01;
-                    C(i, j + 2) = c_02;
-                    C(i, j + 3) = c_03;
-
-                    C(i + 1, j) = c_10;
-                    C(i + 1, j + 1) = c_11;
-                    C(i + 1, j + 2) = c_12;
-                    C(i + 1, j + 3) = c_13;
-
-                    C(i + 2, j) = c_20;
-                    C(i + 2, j + 1) = c_21;
-                    C(i + 2, j + 2) = c_22;
-                    C(i + 2, j + 3) = c_23;
-
-                    C(i + 3, j) = c_30;
-                    C(i + 3, j + 1) = c_31;
-                    C(i + 3, j + 2) = c_32;
-                    C(i + 3, j + 3) = c_33;
-
                     j += 4;
                 }
                 else // only unrolling A
                 {
                     /* Compute C(i,j) */
-                    register float cij_0 = C[i + j * lda];
-                    register float cij_1 = C[i + 1 + j * lda];
-                    register float cij_2 = C[i + 2 + j * lda];
-                    register float cij_3 = C[i + 3 + j * lda];
+                    __m128 c_0 = _mm_loadu_ps(&C(i, 0 + j)); // c_00, c_10, c_20, c_30
+                    float *p_b;
+                    p_b = &B(0, 0 + j);
 
                     for (int k = 0; k < K; ++k)
                     {
-                        register float bkj = B[k + j * lda];
-                        cij_0 += bkj * A[i + k * lda];
-                        cij_1 += bkj * A[i + 1 + k * lda];
-                        cij_2 += bkj * A[i + 2 + k * lda];
-                        cij_3 += bkj * A[i + 3 + k * lda];
-                    }
+                        __m128 a = _mm_loadu_ps(&A(i, k)); // a_0k, a_1k, a_2k, a_3k
+                        __m128 b = _mm_set1_ps(*p_b);
+                        p_b++;
 
-                    C[i + j * lda] = cij_0;
-                    C[i + 1 + j * lda] = cij_1;
-                    C[i + 2 + j * lda] = cij_2;
-                    C[i + 3 + j * lda] = cij_3;
-                    
+                        __m128 c = _mm_loadu_ps(&C(i, 0 + j)); // c_00, c_10, c_20, c_30
+                        c = _mm_add_ps(c, _mm_mul_ps(a, b));
+
+                        _mm_storeu_ps(&C(i, 0 + j), c);
+                    }
                     j += 1;
                 }
             }
@@ -186,7 +198,6 @@ static inline void do_block_divide_4x4(int lda, int M, int N, int K, float *A, f
                     }
 
                     C[i + j * lda] = cij;
-
                     j += 1;
                 }
             }
@@ -198,8 +209,8 @@ static inline void do_block_divide_4x4(int lda, int M, int N, int K, float *A, f
 /* This routine performs a sgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
- * On exit, A and B maintain their input values. 
-*/
+ * On exit, A and B maintain their input values.
+ */
 void square_sgemm(int lda, float *A, float *B, float *C)
 {
     /* For each block-row of A */
@@ -217,9 +228,8 @@ void square_sgemm(int lda, float *A, float *B, float *C)
                 int K = min(BLOCK_SIZE, lda - k);
 
                 /* Perform individual block sgemm */
-                do_block_divide_4x4(lda, M, N, K, A + i + k * lda, B + k + j * lda, C + i + j * lda);
+                do_block_divide_simd(lda, M, N, K, A + i + k * lda, B + k + j * lda, C + i + j * lda);
             }
         }
     }
 }
-
