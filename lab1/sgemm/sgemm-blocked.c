0acbd08aa992ce74f7ff37c3ea7ff5538b411d74
Ther <1329438302@qq.com>
2023-03-14 19:22:13 +0800

    chore: add transpose

--- lab1/sgemm/sgemm-blocked.c
+++ lab1/sgemm/sgemm-blocked.c
@@ -5,7 +5,7 @@
 const char *sgemm_desc = "Simple blocked sgemm.";
 
 #if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 40
+#define BLOCK_SIZE 64
 #endif
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
@@ -32,12 +32,17 @@ static inline void do_block_divide_simd(int lda, int M, int N, int K, float *A, 
             p_b_k2 = &B(0, 2 + j);
             p_b_k3 = &B(0, 3 + j);
 
+            __m128 c_0 = _mm_load_ps(&C(i, 0 + j)); // c_00, c_10, c_20, c_30
+            __m128 c_1 = _mm_load_ps(&C(i, 1 + j)); // c_01, c_11, c_21, c_31
+            __m128 c_2 = _mm_load_ps(&C(i, 2 + j)); // c_02, c_12, c_22, c_32
+            __m128 c_3 = _mm_load_ps(&C(i, 3 + j)); // c_03, c_13, c_23, c_33
+
             for (int k = 0; k < K; k += 4)
             {
-                __m128 a_0 = _mm_load_ps(&A(i, k)); // a_0k, a_1k, a_2k, a_3k
-                __m128 a_1 = _mm_load_ps(&A(i, k + 1));
-                __m128 a_2 = _mm_load_ps(&A(i, k + 2));
-                __m128 a_3 = _mm_load_ps(&A(i, k + 3));
+                __m128 a_0 = _mm_load_ps(&A(4 * k, i / 4)); // a_0k, a_1k, a_2k, a_3k
+                __m128 a_1 = _mm_load_ps(&A(4 * k + 4, i / 4));
+                __m128 a_2 = _mm_load_ps(&A(4 * k + 8, i / 4));
+                __m128 a_3 = _mm_load_ps(&A(4 * k + 12, i / 4));
 
                 // the position of c_00, c_10, c_20, c_30 is continuous
                 __m128 b_00 = _mm_set1_ps(*p_b_k0); // b_1j, b_1j, b_1j, b_1j
@@ -65,11 +70,6 @@ static inline void do_block_divide_simd(int lda, int M, int N, int K, float *A, 
                 p_b_k2 += 4;
                 p_b_k3 += 4;
 
-                __m128 c_0 = _mm_load_ps(&C(i, 0 + j)); // c_00, c_10, c_20, c_30
-                __m128 c_1 = _mm_load_ps(&C(i, 1 + j)); // c_01, c_11, c_21, c_31
-                __m128 c_2 = _mm_load_ps(&C(i, 2 + j)); // c_02, c_12, c_22, c_32
-                __m128 c_3 = _mm_load_ps(&C(i, 3 + j)); // c_03, c_13, c_23, c_33
-
                 c_0 = _mm_add_ps(c_0,
                                  _mm_add_ps(
                                      _mm_add_ps(_mm_mul_ps(a_0, b_00), _mm_mul_ps(a_1, b_01)),
@@ -90,23 +90,26 @@ static inline void do_block_divide_simd(int lda, int M, int N, int K, float *A, 
                                      _mm_add_ps(_mm_mul_ps(a_0, b_30), _mm_mul_ps(a_1, b_31)),
                                      _mm_add_ps(_mm_mul_ps(a_2, b_32), _mm_mul_ps(a_3, b_33))));
 
-                _mm_store_ps(&C(i, 0 + j), c_0);
-                _mm_store_ps(&C(i, 1 + j), c_1);
-                _mm_store_ps(&C(i, 2 + j), c_2);
-                _mm_store_ps(&C(i, 3 + j), c_3);
             }
+            _mm_store_ps(&C(i, 0 + j), c_0);
+            _mm_store_ps(&C(i, 1 + j), c_1);
+            _mm_store_ps(&C(i, 2 + j), c_2);
+            _mm_store_ps(&C(i, 3 + j), c_3);
         }
     }
 }
 
 void copy_memory_continuously(int lda, int M, int N, int K, float *A, float *B, float *C, float *ABC)
 {
-    // copy A
+    // copy A[i + k * lda] to ABC[4 * k + i / 4 * BLOCKSIZE]
     for (int k = 0; k < K; k++)
     {
-        float *p_ABC = ABC + k * BLOCK_SIZE;
-        float *p_A = A + k * lda;
-        memcpy(p_ABC, p_A, M * sizeof(float));
+        for (int i = 0; i < M; i += 4)
+        {
+            float *p_ABC = ABC + i / 4 * BLOCK_SIZE + 4 * k;
+            float *p_A = A + i + k * lda;
+            memcpy(p_ABC, p_A, 4 * sizeof(float));
+        }
     }
 
     // copy B
