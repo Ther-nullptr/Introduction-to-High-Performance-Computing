acb4633ecda9c486407bbbf52c0fdf0b12d575b1
Ther <1329438302@qq.com>
2023-03-15 13:18:48 +0800

    chore: small improvement

--- lab1/sgemm/sgemm-blocked.c
+++ lab1/sgemm/sgemm-blocked.c
@@ -1,11 +1,11 @@
 #include <assert.h>
-#include <immintrin.h>
+#include <emmintrin.h>
 #include <string.h>
 
 const char *sgemm_desc = "Simple blocked sgemm.";
 
 #if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 256
+#define BLOCK_SIZE 260
 #endif
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
@@ -38,59 +38,109 @@ static inline void do_block_divide_simd(int lda, int M, int N, int K, float *A, 
             __m128 c_2 = _mm_load_ps(&C(i, 2 + j)); // c_02, c_12, c_22, c_32
             __m128 c_3 = _mm_load_ps(&C(i, 3 + j)); // c_03, c_13, c_23, c_33
 
+            __m128 a_0, a_1, a_2, a_3;
+            __m128 b_0, b_1, b_2, b_3;
+            __m128 tmp1, tmp2, tmp3, tmp4;
+
+            register float b00, b01, b02, b03;
+            register float b10, b11, b12, b13;
+            register float b20, b21, b22, b23;
+            register float b30, b31, b32, b33;
+
             for (int k = 0; k < K; k += 4)
             {
-                __m128 a_0 = _mm_load_ps(&A(4 * k, i)); // a_0k, a_1k, a_2k, a_3k
-                __m128 a_1 = _mm_load_ps(&A(4 * k + 4, i));
-                __m128 a_2 = _mm_load_ps(&A(4 * k + 8, i));
-                __m128 a_3 = _mm_load_ps(&A(4 * k + 12, i));
+                b00 = *p_b_k0;
+                b01 = *(p_b_k0 + 1);
+                b02 = *(p_b_k0 + 2);
+                b03 = *(p_b_k0 + 3);
+
+                b10 = *p_b_k1;
+                b11 = *(p_b_k1 + 1);
+                b12 = *(p_b_k1 + 2);
+                b13 = *(p_b_k1 + 3);
+
+                b20 = *p_b_k2;
+                b21 = *(p_b_k2 + 1);
+                b22 = *(p_b_k2 + 2);
+                b23 = *(p_b_k2 + 3);
+
+                b30 = *p_b_k3;
+                b31 = *(p_b_k3 + 1);
+                b32 = *(p_b_k3 + 2);
+                b33 = *(p_b_k3 + 3);
+
+                a_0 = _mm_load_ps(&A(4 * k, i)); // a_0k, a_1k, a_2k, a_3k
+                a_1 = _mm_load_ps(&A(4 * k + 4, i));
+                a_2 = _mm_load_ps(&A(4 * k + 8, i));
+                a_3 = _mm_load_ps(&A(4 * k + 12, i));
 
                 // the position of c_00, c_10, c_20, c_30 is continuous
-                __m128 b_00 = _mm_set1_ps(*p_b_k0); // b_1j, b_1j, b_1j, b_1j
-                __m128 b_10 = _mm_set1_ps(*p_b_k1); // b_2j, b_2j, b_2j, b_2j
-                __m128 b_20 = _mm_set1_ps(*p_b_k2); // b_3j, b_3j, b_3j, b_3j
-                __m128 b_30 = _mm_set1_ps(*p_b_k3); // b_4j, b_4j, b_4j, b_4j
+                b_0 = _mm_set1_ps(b00); // b_1j, b_1j, b_1j, b_1j
+                b_1 = _mm_set1_ps(b01); // b_1j, b_1j, b_1j, b_1j
+                b_2 = _mm_set1_ps(b02); // b_1j, b_1j, b_1j, b_1j
+                b_3 = _mm_set1_ps(b03); // b_1j, b_1j, b_1j, b_1j
 
-                __m128 b_01 = _mm_set1_ps(*(p_b_k0 + 1)); // b_1j, b_1j, b_1j, b_1j
-                __m128 b_11 = _mm_set1_ps(*(p_b_k1 + 1)); // b_2j, b_2j, b_2j, b_2j
-                __m128 b_21 = _mm_set1_ps(*(p_b_k2 + 1)); // b_3j, b_3j, b_3j, b_3j
-                __m128 b_31 = _mm_set1_ps(*(p_b_k3 + 1)); // b_4j, b_4j, b_4j, b_4j
+                tmp1 = _mm_mul_ps(a_0, b_0);
+                tmp2 = _mm_mul_ps(a_1, b_1);
+                tmp3 = _mm_mul_ps(a_2, b_2);
+                tmp4 = _mm_mul_ps(a_3, b_3);
 
-                __m128 b_02 = _mm_set1_ps(*(p_b_k0 + 2)); // b_1j, b_1j, b_1j, b_1j
-                __m128 b_12 = _mm_set1_ps(*(p_b_k1 + 2)); // b_2j, b_2j, b_2j, b_2j
-                __m128 b_22 = _mm_set1_ps(*(p_b_k2 + 2)); // b_3j, b_3j, b_3j, b_3j
-                __m128 b_32 = _mm_set1_ps(*(p_b_k3 + 2)); // b_4j, b_4j, b_4j, b_4j
+                c_0 = _mm_add_ps(c_0,
+                                 _mm_add_ps(
+                                     _mm_add_ps(tmp1, tmp2),
+                                     _mm_add_ps(tmp3, tmp4)));
 
-                __m128 b_03 = _mm_set1_ps(*(p_b_k0 + 3)); // b_1j, b_1j, b_1j, b_1j
-                __m128 b_13 = _mm_set1_ps(*(p_b_k1 + 3)); // b_2j, b_2j, b_2j, b_2j
-                __m128 b_23 = _mm_set1_ps(*(p_b_k2 + 3)); // b_3j, b_3j, b_3j, b_3j
-                __m128 b_33 = _mm_set1_ps(*(p_b_k3 + 3)); // b_4j, b_4j, b_4j, b_4j
+                b_0 = _mm_set1_ps(b10); // b_2j, b_2j, b_2j, b_2j
+                b_1 = _mm_set1_ps(b11); // b_2j, b_2j, b_2j, b_2j
+                b_2 = _mm_set1_ps(b12); // b_2j, b_2j, b_2j, b_2j
+                b_3 = _mm_set1_ps(b13); // b_2j, b_2j, b_2j, b_2j
+
+                tmp1 = _mm_mul_ps(a_0, b_0);
+                tmp2 = _mm_mul_ps(a_1, b_1);
+                tmp3 = _mm_mul_ps(a_2, b_2);
+                tmp4 = _mm_mul_ps(a_3, b_3);
+
+                c_1 = _mm_add_ps(c_1,
+                                 _mm_add_ps(
+                                     _mm_add_ps(tmp1, tmp2),
+                                     _mm_add_ps(tmp3, tmp4)));
+
+                b_0 = _mm_set1_ps(b20); // b_3j, b_3j, b_3j, b_3j
+                b_1 = _mm_set1_ps(b21); // b_3j, b_3j, b_3j, b_3j
+                b_2 = _mm_set1_ps(b22); // b_3j, b_3j, b_3j, b_3j
+                b_3 = _mm_set1_ps(b23); // b_3j, b_3j, b_3j, b_3j
+
+                tmp1 = _mm_mul_ps(a_0, b_0);
+                tmp2 = _mm_mul_ps(a_1, b_1);
+                tmp3 = _mm_mul_ps(a_2, b_2);
+                tmp4 = _mm_mul_ps(a_3, b_3);
+
+                c_2 = _mm_add_ps(c_2,
+                                 _mm_add_ps(
+                                     _mm_add_ps(tmp1, tmp2),
+                                     _mm_add_ps(tmp3, tmp4)));
+
+                b_0 = _mm_set1_ps(b30); // b_4j, b_4j, b_4j, b_4j
+                b_1 = _mm_set1_ps(b31); // b_4j, b_4j, b_4j, b_4j
+                b_2 = _mm_set1_ps(b32); // b_4j, b_4j, b_4j, b_4j
+                b_3 = _mm_set1_ps(b33); // b_4j, b_4j, b_4j, b_4j
+
+                tmp1 = _mm_mul_ps(a_0, b_0);
+                tmp2 = _mm_mul_ps(a_1, b_1);
+                tmp3 = _mm_mul_ps(a_2, b_2);
+                tmp4 = _mm_mul_ps(a_3, b_3);
+
+                c_3 = _mm_add_ps(c_3,
+                                 _mm_add_ps(
+                                     _mm_add_ps(tmp1, tmp2),
+                                     _mm_add_ps(tmp3, tmp4)));
 
                 p_b_k0 += 4;
                 p_b_k1 += 4;
                 p_b_k2 += 4;
-                p_b_k3 += 4;
-
-                c_0 = _mm_add_ps(c_0,
-                                 _mm_add_ps(
-                                     _mm_add_ps(_mm_mul_ps(a_0, b_00), _mm_mul_ps(a_1, b_01)),
-                                     _mm_add_ps(_mm_mul_ps(a_2, b_02), _mm_mul_ps(a_3, b_03))));
-
-                c_1 = _mm_add_ps(c_1,
-                                 _mm_add_ps(
-                                     _mm_add_ps(_mm_mul_ps(a_0, b_10), _mm_mul_ps(a_1, b_11)),
-                                     _mm_add_ps(_mm_mul_ps(a_2, b_12), _mm_mul_ps(a_3, b_13))));
-
-                c_2 = _mm_add_ps(c_2,
-                                 _mm_add_ps(
-                                     _mm_add_ps(_mm_mul_ps(a_0, b_20), _mm_mul_ps(a_1, b_21)),
-                                     _mm_add_ps(_mm_mul_ps(a_2, b_22), _mm_mul_ps(a_3, b_23))));
-
-                c_3 = _mm_add_ps(c_3,
-                                 _mm_add_ps(
-                                     _mm_add_ps(_mm_mul_ps(a_0, b_30), _mm_mul_ps(a_1, b_31)),
-                                     _mm_add_ps(_mm_mul_ps(a_2, b_32), _mm_mul_ps(a_3, b_33))));
+                p_b_k3 += 4; 
             }
+
             _mm_store_ps(&C(i, 0 + j), c_0);
             _mm_store_ps(&C(i, 1 + j), c_1);
             _mm_store_ps(&C(i, 2 + j), c_2);
@@ -102,47 +152,51 @@ static inline void do_block_divide_simd(int lda, int M, int N, int K, float *A, 
 static inline void copy_memory_continuously(int lda, int M, int N, int K, float *A, float *B, float *C, float *ABC)
 {
     // copy A
+    float *p_ABC = ABC;
+    float *p_A = A;
     for (int k = 0; k < K; k++)
     {
-        float *p_ABC = ABC + k * BLOCK_SIZE;
-        float *p_A = A + k * lda;
         memcpy(p_ABC, p_A, M * sizeof(float));
+        p_ABC += BLOCK_SIZE;
+        p_A += lda;
     }
 
     // copy B
+    p_ABC = ABC + BLOCK_SIZE * BLOCK_SIZE;
+    float *p_B = B;
     for (int j = 0; j < N; j++)
     {
-        float *p_ABC = ABC + j * BLOCK_SIZE + BLOCK_SIZE * BLOCK_SIZE;
-        float *p_B = B + j * lda;
         memcpy(p_ABC, p_B, K * sizeof(float));
+        p_ABC += BLOCK_SIZE;
+        p_B += lda;
     }
 
     // copy C
+    p_ABC = ABC + 2 * BLOCK_SIZE * BLOCK_SIZE;
+    float *p_C = C;
     for (int j = 0; j < N; j++)
     {
-        float *p_ABC = ABC + j * BLOCK_SIZE + 2 * BLOCK_SIZE * BLOCK_SIZE;
-        float *p_C = C + j * lda;
         memcpy(p_ABC, p_C, M * sizeof(float));
+        p_ABC += BLOCK_SIZE;
+        p_C += lda;
     }
 }
 
-static inline void transpose_A(int lda, int M, int N, int K, float* ABC)
+static inline void transpose_A(int lda, int M, int N, int K, float* ABC, float* temp_array)
 {
     // transpose A in ABC
-    float temp_array[BLOCK_SIZE * BLOCK_SIZE] __attribute__((aligned(64)));
     for (int i = 0; i < M; i += 4)
     {
         for (int k = 0; k < K; k += 4)
         {
-            __m128 temp1 = _mm_load_ps(ABC + k * BLOCK_SIZE + i);
-            __m128 temp2 = _mm_load_ps(ABC + (k + 1) * BLOCK_SIZE + i);
-            __m128 temp3 = _mm_load_ps(ABC + (k + 2) * BLOCK_SIZE + i);
-            __m128 temp4 = _mm_load_ps(ABC + (k + 3) * BLOCK_SIZE + i);
-
-            _mm_store_ps(&temp_array(4 * k, i), temp1);
-            _mm_store_ps(&temp_array(4 * k + 4, i), temp2);
-            _mm_store_ps(&temp_array(4 * k + 8, i), temp3);
-            _mm_store_ps(&temp_array(4 * k + 12, i), temp4);
+            __m128 temp = _mm_load_ps(ABC + k * BLOCK_SIZE + i);
+            _mm_store_ps(&temp_array(4 * k, i), temp);
+            temp = _mm_load_ps(ABC + (k + 1) * BLOCK_SIZE + i);
+            _mm_store_ps(&temp_array(4 * k + 4, i), temp);
+            temp = _mm_load_ps(ABC + (k + 2) * BLOCK_SIZE + i);
+            _mm_store_ps(&temp_array(4 * k + 8, i), temp);
+            temp = _mm_load_ps(ABC + (k + 3) * BLOCK_SIZE + i);
+            _mm_store_ps(&temp_array(4 * k + 12, i), temp);
         }
     }
     memcpy(ABC, temp_array, BLOCK_SIZE * BLOCK_SIZE * sizeof(float));
@@ -166,6 +220,7 @@ static inline void write_back(int lda, int M, int N, int K, float *C, float *ABC
 void square_sgemm(int lda, float *A, float *B, float *C)
 {
     float ABC[3 * BLOCK_SIZE * BLOCK_SIZE] __attribute__((aligned(64))); // A,B,C blocks in same matrix
+    float temp_array[BLOCK_SIZE * BLOCK_SIZE] __attribute__((aligned(64)));
     /* For each block-row of A */
     for (int j = 0; j < lda; j += BLOCK_SIZE)
     {
@@ -183,12 +238,15 @@ void square_sgemm(int lda, float *A, float *B, float *C)
                 int M = min(BLOCK_SIZE, lda - i);
                 int M_padding = (M % 4 == 0) ? M : ((M / 4 + 1) * 4);
                 
-                memset(ABC, 0, sizeof(ABC));
+                if (lda > BLOCK_SIZE)
+                {
+                    memset(ABC, 0, sizeof(ABC));
+                }
 
                 /* copy the A\B\C matrix into continue memory */
                 copy_memory_continuously(lda, M, N, K, A + i + k * lda, B + k + j * lda, C + i + j * lda, ABC);
 
-                transpose_A(BLOCK_SIZE, M_padding, N_padding, K_padding, ABC);
+                transpose_A(BLOCK_SIZE, M_padding, N_padding, K_padding, ABC, temp_array);
                 /* perform individual block sgemm */
                 do_block_divide_simd(BLOCK_SIZE, M_padding, N_padding, K_padding, ABC, ABC + BLOCK_SIZE * BLOCK_SIZE, ABC + 2 * BLOCK_SIZE * BLOCK_SIZE);
 
