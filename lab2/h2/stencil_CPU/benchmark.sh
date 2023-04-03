c77d8159b4cc661ddbb98da9fb415bf447e501a2
Ther <1329438302@qq.com>
2023-04-03 21:35:38 +0800

    chore: modify command

--- lab2/h2/stencil_CPU/benchmark.sh
+++ lab2/h2/stencil_CPU/benchmark.sh
@@ -13,6 +13,6 @@ export DAPL_DBG_TYPE=0
 
 DATAPATH=/home/2023-spring/data/stencil_data
 
-# env OMP_NUM_THREADS=$2 ./$1 27 256 256 256 16 ${DATAPATH}/stencil_data_256x256x256
+env OMP_NUM_THREADS=$2 ./$1 27 256 256 256 16 ${DATAPATH}/stencil_data_256x256x256
 # env OMP_NUM_THREADS=$2 ./$1 27 384 384 384 16 ${DATAPATH}/stencil_data_384x384x384
-env OMP_NUM_THREADS=$2 ./$1 27 512 512 512 16 ${DATAPATH}/stencil_data_512x512x512
+# env OMP_NUM_THREADS=$2 ./$1 27 512 512 512 16 ${DATAPATH}/stencil_data_512x512x512
