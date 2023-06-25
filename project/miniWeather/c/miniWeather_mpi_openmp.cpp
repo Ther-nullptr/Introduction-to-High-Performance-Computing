cf723f87741ea9573142c9434e8e36f2a2dd3f24
Ther <1329438302@qq.com>
2023-06-25 14:28:18 +0800

    chore: add naive pow

--- project/miniWeather/c/miniWeather_mpi_openmp.cpp
+++ project/miniWeather/c/miniWeather_mpi_openmp.cpp
@@ -126,16 +126,9 @@ void   set_halo_values_x    ( double *state );
 void   set_halo_values_z    ( double *state );
 void   reductions           ( double &mass , double &te );
 
-inline double fastpow(double a, const double b)
+inline double fastpow(double a, double b)
 {
-    union
-    {
-        double d;
-        int x[2];
-    } u = {a};
-    u.x[1] = (int)(b * (u.x[1] - 1072632447) + 1072632447);
-    u.x[0] = 0;
-    return u.d;
+    return pow(a, b);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////
